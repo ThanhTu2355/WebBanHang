683d20097dd2ca7eb5e9eabaa6de7d68233865ee
nguyentu1112005 <nguyentu1112005@gmail.com>
2025-05-16 14:07:28 +0700

    update

--- WebBanHang/Controllers/ProductController.cs
+++ WebBanHang/Controllers/ProductController.cs
@@ -66,6 +66,66 @@ namespace WebBanHang.Controllers
             return View();
         }
 
+        //Xử lý cập nhật sản phẩm
+        [HttpPost]
+        public IActionResult Update(Product product, IFormFile ImageUrl)
+        {
+            if (ModelState.IsValid) //kiem tra hop le
+            {
+                var existingProduct = _db.Products.Find(product.Id);
+                if (ImageUrl != null)
+                {
+                    //xu ly upload và lưu ảnh đại diện mới
+                    product.ImageUrl = SaveImage(ImageUrl);
+                    //xóa ảnh cũ (nếu có)
+                    if (!string.IsNullOrEmpty(existingProduct.ImageUrl))
+                    {
+                        var oldFilePath = Path.Combine(_hosting.WebRootPath, existingProduct.ImageUrl);
+                        if (System.IO.File.Exists(oldFilePath))
+                        {
+                            System.IO.File.Delete(oldFilePath);
+                        }
+                    }
+                }
+                else
+                {
+                    product.ImageUrl = existingProduct.ImageUrl;
+                }
+                //cập nhật product vào table Product
+                existingProduct.Name = product.Name;
+                existingProduct.Description = product.Description;
+                existingProduct.Price = product.Price;
+                existingProduct.CategoryId = product.CategoryId;
+                existingProduct.ImageUrl = product.ImageUrl;
+                _db.SaveChanges();
+                TempData["success"] = "Product updated success";
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View();
+        }
+
+        //Hiển thị form cập nhật sản phẩm
+        public IActionResult Update(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            //truyền danh sách thể loại cho View để sinh ra điều khiển DropDownList
+            ViewBag.CategoryList = _db.Categories.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name
+            });
+            return View(product);
+        }
+
         private string SaveImage(IFormFile image)
         {
             //đặt lại tên file cần lưu
@@ -78,6 +138,42 @@ namespace WebBanHang.Controllers
                 image.CopyTo(filestream);
             }
             return @"images/products/" + filename;
-        } 
+        }
+
+        //Hiển thị form xác nhận xóa sản phẩm
+        public IActionResult Delete(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        //Xử lý xóa sản phẩm
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            // xoá hình cũ của sản phẩm
+            if (!String.IsNullOrEmpty(product.ImageUrl))
+            {
+                var oldFilePath = Path.Combine(_hosting.WebRootPath, product.ImageUrl);
+                if (System.IO.File.Exists(oldFilePath))
+                {
+                    System.IO.File.Delete(oldFilePath);
+                }
+            }
+            // xoa san pham khoi CSDL
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            TempData["success"] = "Product deleted success";
+            //chuyen den action index
+            return RedirectToAction("Index");
+        }
     }
 }
