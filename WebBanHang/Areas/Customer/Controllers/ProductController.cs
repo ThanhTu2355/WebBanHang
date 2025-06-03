7ecf6ccdd0f225ab0857ab226056c8c262c11344
nguyentu1112005 <nguyentu1112005@gmail.com>
2025-06-03 16:41:26 +0700

    update code

--- WebBanHang/Areas/Customer/Controllers/ProductController.cs
+++ WebBanHang/Areas/Customer/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebBanHang.Areas.Customer.Models;
 using WebBanHang.Models;
 
 namespace WebBanHang.Areas.Customer.Controllers
@@ -13,70 +14,37 @@ namespace WebBanHang.Areas.Customer.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
-        private readonly IWebHostEnvironment _hosting;
-        public ProductController(ApplicationDbContext db, IWebHostEnvironment hosting)
+        public ProductController(ApplicationDbContext db)
         {
             _db = db;
-            _hosting = hosting;
         }
-        public IActionResult Index(int? categoryId = 1)
+        public IActionResult Index(int categoryId = 1)
         {
-            List<Product> products;
-            if (categoryId.HasValue)
-            {
-                products = _db.Products.Where(p => p.CategoryId == categoryId.Value).ToList();
-            }
-            else
-            {
-                products = _db.Products.ToList();
-            }
-            var categories = _db.Categories.ToList();
-            var countProduct = _db.Products
-                                   .GroupBy(p => p.CategoryId)
-                                   .Select(g => new
-                                   {
-                                       CategoryId = g.Key,
-                                       Count = g.Count()
-                                   })
-                                   .ToDictionary(x => x.CategoryId, x => x.Count);
+            var categories = _db.Categories.
+                             Select(c => new CategoryViewModel
+                             { Id = c.Id, Name = c.Name, TotalProduct = _db.Products.Where(p => p.CategoryId == c.Id).Count() }).ToList();
+            var products = _db.Products.Where(p => p.CategoryId == categoryId).ToList();
 
-
-            ViewBag.Products = products;
             ViewBag.Categories = categories;
+            ViewBag.Products = products;
+            ViewBag.Title = _db.Categories.Find(categoryId).Name;
             ViewBag.SelectedCategoryId = categoryId;
-            ViewBag.CountProduct = countProduct;
 
             return View();
         }
 
         // Ajax: Lấy sản phẩm theo category
         [HttpGet]
-        public IActionResult Ajax(int? categoryId = 1)
+        public IActionResult Ajax(int categoryId = 1)
         {
-            List<Product> products;
-            if (categoryId.HasValue)
-            {
-                products = _db.Products.Where(p => p.CategoryId == categoryId.Value).ToList();
-            }
-            else
-            {
-                products = _db.Products.ToList();
-            }
-            var categories = _db.Categories.ToList();
-            var countProduct = _db.Products
-                                   .GroupBy(p => p.CategoryId)
-                                   .Select(g => new
-                                   {
-                                       CategoryId = g.Key,
-                                       Count = g.Count()
-                                   })
-                                   .ToDictionary(x => x.CategoryId, x => x.Count);
+            var categories = _db.Categories.
+                             Select(c => new CategoryViewModel
+                             { Id = c.Id, Name = c.Name, TotalProduct = _db.Products.Where(p => p.CategoryId == c.Id).Count() }).ToList();
+            var products = _db.Products.Where(p => p.CategoryId == categoryId).ToList();
 
-
-            ViewBag.Products = products;
             ViewBag.Categories = categories;
+            ViewBag.Products = products;
             ViewBag.SelectedCategoryId = categoryId;
-            ViewBag.CountProduct = countProduct;
 
             return PartialView("_ProductListPartial", products);
         }
